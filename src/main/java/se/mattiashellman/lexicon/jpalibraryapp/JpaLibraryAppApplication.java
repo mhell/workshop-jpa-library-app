297da9e7eaabb415a1e6bcb670b793c960c38a88
Mattias Hellman <mattias.hellman@gmail.com>
2026-01-14 10:12:54 +0100

    Set up database.

---
+++ src/main/java/se/mattiashellman/lexicon/jpalibraryapp/JpaLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package se.mattiashellman.lexicon.jpalibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLibraryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaLibraryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/mattiashellman/lexicon/jpalibraryapp/JpaLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package se.mattiashellman.lexicon.jpalibraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLibraryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
