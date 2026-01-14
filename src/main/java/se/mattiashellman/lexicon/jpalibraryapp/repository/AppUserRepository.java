c70a1f20cebfcb2bbb15771260201fd2d9b7676b
Mattias Hellman <mattias.hellman@gmail.com>
2026-01-14 13:46:03 +0100

    Add repositories

---
+++ src/main/java/se/mattiashellman/lexicon/jpalibraryapp/repository/AppUserRepository.java
@@ -0,0 +1,24 @@
+package se.mattiashellman.lexicon.jpalibraryapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import se.mattiashellman.lexicon.jpalibraryapp.entity.AppUser;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+
+    Optional<AppUser> findByUsername(String username);
+    
+    List<AppUser> findByRegDateBetween(LocalDate regDateAfter, LocalDate regDateBefore);
+
+    List<AppUser> findByUserDetails_Id(int userDetailsId);
+
+    // @Query("SELECT u FROM AppUser u WHERE LOWER(u.userDetails.email) = LOWER(:email)")
+    // Optional<AppUser> findByEmailIgnoreCase(@Param("email") String email);
+
+    Optional<AppUser> findByUserDetails_EmailIgnoreCase(String email);
+}
---
+++ src/main/java/se/mattiashellman/lexicon/jpalibraryapp/repository/DetailsRepository.java
@@ -0,0 +1,15 @@
+package se.mattiashellman.lexicon.jpalibraryapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.mattiashellman.lexicon.jpalibraryapp.entity.Details;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DetailsRepository extends JpaRepository<Details, Integer> {
+
+    Optional<Details> findByEmail(String email);
+    List<Details> findByNameContains(String name);
+    List<Details> findByNameIgnoreCase(String name);
+    
+}
