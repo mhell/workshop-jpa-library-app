cb390d4ec3af335b885f42eb50a7513e2f597a94
Mattias Hellman <mattias.hellman@gmail.com>
2026-01-22 11:51:22 +0100

    Implements Book and BookLoan repos

---
+++ src/main/java/se/mattiashellman/lexicon/jpalibraryapp/repository/BookLoanRepository.java
@@ -0,0 +1,41 @@
+package se.mattiashellman.lexicon.jpalibraryapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import se.mattiashellman.lexicon.jpalibraryapp.entity.Book;
+import se.mattiashellman.lexicon.jpalibraryapp.entity.BookLoan;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface BookLoanRepository extends JpaRepository<BookLoan, Integer> {
+
+    // Find by borrower's ID.
+    List<BookLoan> findByBorrower_Id(int borrowerId);
+
+    // Find by book ID.
+    List<BookLoan> findByBook_Id(int bookId);
+
+    // Find all book loans that have not been returned yet.
+    List<BookLoan> findByReturnedFalse(); // TODO: test
+
+    // Find all overdue book loans.
+    List<BookLoan> findByDueDateIsAfter(LocalDate dueDateAfter);
+
+    default List<BookLoan> findByIsOverdue() {
+        return findByDueDateIsAfter(LocalDate.now());
+    }
+
+    // @Query("SELECT b FROM BookLoan b WHERE b.dueDate > CURRENT_TIMESTAMP")
+    // List<BookLoan> findByIsOverdue();
+
+    // Find loans between dates.
+    List<BookLoan> findByLoanDateBetween(LocalDate loanDateAfter, LocalDate loanDateBefore);
+
+    // Mark a book loan as returned by its loan ID.
+    @Modifying
+    @Query("UPDATE BookLoan l SET l.returned = true WHERE l.id = :id")
+    void returnBook(int id);
+}
---
+++ src/main/java/se/mattiashellman/lexicon/jpalibraryapp/repository/BookRepository.java
@@ -0,0 +1,16 @@
+package se.mattiashellman.lexicon.jpalibraryapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.mattiashellman.lexicon.jpalibraryapp.entity.Book;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    
+    List<Book> findByIsbnIgnoreCase(String isbn);
+
+    List<Book> findByTitleContains(String title);
+
+    List<Book> findByMaxLoanDaysIsLessThan(int days);
+
+}
