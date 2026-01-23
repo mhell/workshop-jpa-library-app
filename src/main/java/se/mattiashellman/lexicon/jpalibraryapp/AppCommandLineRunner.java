7a83b70424fc4a6146ec18d6700cb9e19b6da4c0
Mattias Hellman <mattias.hellman@gmail.com>
2026-01-23 11:37:37 +0100

    Add comments

--- src/main/java/se/mattiashellman/lexicon/jpalibraryapp/AppCommandLineRunner.java
+++ src/main/java/se/mattiashellman/lexicon/jpalibraryapp/AppCommandLineRunner.java
@@ -1,5 +1,6 @@
 package se.mattiashellman.lexicon.jpalibraryapp;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -25,40 +26,50 @@ public class AppCommandLineRunner implements CommandLineRunner {
     }
 
     @Override
+    @Transactional
     public void run(String... args){
         // AppUserRepository
-        // Details details = detailsRepository.save(new Details("email@email.com", "name", LocalDate.of(1982, 12, 2)));
-        // appUserRepository.save(new AppUser("username", "123", LocalDate.now(), details));
-        //
-        // Optional<AppUser>  appUser = appUserRepository.findByUsername("username");
-        // System.out.print("1. ");
-        // appUser.ifPresent(System.out::println);
-        //
-        // LocalDate regDateAfter = LocalDate.of(0, 1, 1);
-        // LocalDate regDateBefore = LocalDate.of(2030, 1, 1);
-        // List<AppUser> appUsers = appUserRepository.findByRegDateBetween(regDateAfter, regDateBefore);
-        // System.out.print("2. ");
-        // System.out.println(appUsers);
-        //
-        // List<AppUser> appUsers2 = appUserRepository.findByUserDetails_Id(1);
-        // System.out.print("3. ");
-        // System.out.println(appUsers2);
-        //
-        // Optional<AppUser> appUser2 = appUserRepository.findByUserDetails_EmailIgnoreCase("email@email.com");
-        // System.out.print("4. ");
-        // appUser2.ifPresent(System.out::println);
+        Details details = new Details("email@email.com", "name", LocalDate.of(1982, 12, 2));
+        appUserRepository.save(new AppUser("username", "123", LocalDate.now(), details));
+
+        Optional<AppUser> appUser = appUserRepository.findByUsername("username");
+        System.out.print("1. ");
+        appUser.ifPresent(System.out::println);
+
+        LocalDate regDateAfter = LocalDate.of(0, 1, 1);
+        LocalDate regDateBefore = LocalDate.of(2030, 1, 1);
+        List<AppUser> appUsers = appUserRepository.findByRegDateBetween(regDateAfter, regDateBefore);
+        System.out.print("2. ");
+        System.out.println(appUsers);
+
+        List<AppUser> appUsers2 = appUserRepository.findByUserDetails_Id(1);
+        System.out.print("3. ");
+        System.out.println(appUsers2);
+
+        Optional<AppUser> appUser2 = appUserRepository.findByUserDetails_EmailIgnoreCase("email@email.com");
+        System.out.print("4. ");
+        appUser2.ifPresent(System.out::println);
 
         // DetailsRepository
-        // System.out.print("5. ");
-        // Optional<Details> details1 = detailsRepository.findByEmail("email@email.com");
-        // details1.ifPresent(System.out::println);
-        //
-        // List<Details> detailsList = detailsRepository.findByNameContains("n");
-        // System.out.print("6. ");
-        // System.out.println(detailsList);
-        //
-        // List<Details> detailsList1 = detailsRepository.findByNameIgnoreCase("NAME");
-        // System.out.print("7. ");
-        // System.out.println(detailsList1);
+        System.out.print("5. ");
+        Optional<Details> details1 = detailsRepository.findByEmail("email@email.com");
+        details1.ifPresent(System.out::println);
+
+        List<Details> detailsList = detailsRepository.findByNameContains("n");
+        System.out.print("6. ");
+        System.out.println(detailsList);
+
+        List<Details> detailsList1 = detailsRepository.findByNameIgnoreCase("NAME");
+        System.out.print("7. ");
+        System.out.println(detailsList1);
+
+        // AuthorRepository
+
+
+        // BookLoanRepository
+
+
+        // BookRepository
+
     }
 }
